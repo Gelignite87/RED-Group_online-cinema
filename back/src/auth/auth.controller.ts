8b0bac3fc501eb3057bc22bcc109a0eb5a73af94
Gelignite87 <railgun87@rambler.ru>
2023-04-09 15:07:55 +1300

    09.04.2023

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,11 +1,29 @@
 import { AuthService } from './auth.service'
-import { Body, Controller, Post } from '@nestjs/common'
+import {
+  HttpCode,
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common'
+import { AuthDto } from './dto/auth.dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly AuthService: AuthService) {}
+
+  @UsePipes(new ValidationPipe()) //валидация
+  @HttpCode(200) //меняем дефолтный статус ответа сервера
+  @Post('login')
+  async login(@Body() dto: AuthDto) {
+    return this.AuthService.login(dto)
+  }
+
+  @UsePipes(new ValidationPipe()) //валидация
+  @HttpCode(200) //меняем дефолтный статус ответа сервера
   @Post('register')
-  async register(@Body() dto: any) {
+  async register(@Body() dto: AuthDto) {
     return this.AuthService.register(dto)
   }
 }
--- back/src/user/user.model.ts
+++ back/src/user/user.model.ts
@@ -1,10 +1,10 @@
 import { prop } from '@typegoose/typegoose'
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
 
-export interface UserModel extends Base {} //extends Base добавляем поля автоматически создаваемые MongoDB
+export interface UserModel extends Base {} //создаем интерфейс
 
 export class UserModel extends TimeStamps {
-  //extends TimeStamps добавляем поля при любом обновлении
+  //extends TimeStamps добавляем дефолтные поля при любом обновлении
   @prop({ unique: true })
   email: string
 
