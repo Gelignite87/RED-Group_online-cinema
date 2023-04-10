4835e7000e500ac7e191d96dd4a74d297135d2d4
Gelignite87 <railgun87@rambler.ru>
2023-04-10 21:28:34 +1300

    10.04.2023

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -8,19 +8,27 @@ import {
   ValidationPipe,
 } from '@nestjs/common'
 import { AuthDto } from './dto/auth.dto'
+import { RefreshTokenDto } from './dto/refreshToken.dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly AuthService: AuthService) {}
 
-  @UsePipes(new ValidationPipe()) //валидация
+  @UsePipes(new ValidationPipe()) //валидация из файла dto
   @HttpCode(200) //меняем дефолтный статус ответа сервера
   @Post('login')
   async login(@Body() dto: AuthDto) {
     return this.AuthService.login(dto)
   }
 
-  @UsePipes(new ValidationPipe()) //валидация
+  @UsePipes(new ValidationPipe()) //валидация из файла dto
+  @HttpCode(200) //меняем дефолтный статус ответа сервера
+  @Post('login/access-token')
+  async getNewTokens(@Body() dto: RefreshTokenDto) {
+    return this.AuthService.getNewTokens(dto)
+  }
+
+  @UsePipes(new ValidationPipe()) //валидация из файла dto
   @HttpCode(200) //меняем дефолтный статус ответа сервера
   @Post('register')
   async register(@Body() dto: AuthDto) {
