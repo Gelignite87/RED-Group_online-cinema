d47e2459e2f9d09c6ac9611cd300d22768df4a59
Gelignite87 <railgun87@rambler.ru>
2023-04-13 22:26:34 +1300

    13.04.2023

--- back/src/movie/movie.controller.ts
+++ back/src/movie/movie.controller.ts
@@ -1,4 +1,88 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { IdValidationPipe } from 'src/pipes/id.validation.pipe'
+import { MovieService } from './movie.service'
+import { UpdateMovieDto } from './update-movie.dto'
+import { Types } from 'mongoose'
 
-@Controller('movie')
-export class MovieController {}
+@Controller('movies')
+export class MovieController {
+  constructor(private readonly MovieService: MovieService) {}
+
+  @Get('by-slug/:slug')
+  async bySlug(@Param('slug') slug: string) {
+    return this.MovieService.bySlug(slug)
+  }
+
+  @Get('by-actor/:actorId')
+  async byActor(@Param('actorId', IdValidationPipe) actorId: Types.ObjectId) {
+    return this.MovieService.byActor(actorId)
+  }
+
+  @Post('by-genres')
+  @HttpCode(200)
+  async byGenres(@Body('genresIds') genresIds: Types.ObjectId[]) {
+    return this.MovieService.byGenres(genresIds)
+  }
+
+  @Get()
+  async getAll(@Query('searchTerm') searchTerm?: string) {
+    return this.MovieService.getAll(searchTerm)
+  }
+
+  @Get('most-popular')
+  async getMostPopular() {
+    return this.MovieService.getMostPopular()
+  }
+
+  @Put('update-count-opened')
+  @HttpCode(200)
+  async updateCountOpened(@Body('slug') slug: string) {
+    //через Body забираем из объекта только значение ключа slug, если прописать dto то будет подсказка если мы не укажем в запросе slug
+    return this.MovieService.updateCountOpened(slug)
+  }
+
+  @Get(':_id')
+  @Auth('admin')
+  async get(@Param('_id', IdValidationPipe) _id: string) {
+    return this.MovieService.byId(_id)
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post()
+  @HttpCode(200)
+  @Auth('admin')
+  async create() {
+    return this.MovieService.create()
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put(':id')
+  @HttpCode(200)
+  @Auth('admin')
+  async update(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() dto: UpdateMovieDto
+  ) {
+    return this.MovieService.update(id, dto)
+  }
+
+  @Delete(':id')
+  @HttpCode(200)
+  @Auth('admin')
+  async delete(@Param('id', IdValidationPipe) id: string) {
+    return this.MovieService.delete(id)
+  }
+}
---
+++ back/src/movie/update-movie.dto.ts
@@ -0,0 +1,52 @@
+import {
+  IsArray,
+  IsBoolean,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsString,
+} from 'class-validator'
+
+export class Parameters {
+  @IsNumber()
+  year: number
+
+  @IsNumber()
+  duration: number
+
+  @IsString()
+  country: string
+}
+
+export class UpdateMovieDto {
+  @IsString()
+  poster: string
+
+  @IsString()
+  bigPoster: string
+
+  @IsString()
+  title: string
+
+  @IsString()
+  slug: string
+
+  @IsOptional()
+  @IsObject()
+  parameters?: Parameters
+
+  @IsString()
+  videoUrl: string
+
+  @IsArray()
+  @IsString({ each: true })
+  genres: string[]
+
+  @IsArray()
+  @IsString({ each: true }) //каждый элемент массива string
+  actors: string[]
+
+  @IsOptional()
+  @IsBoolean()
+  isSendTelegram?: boolean
+}
