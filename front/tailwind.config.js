3175db3373e8d120bba51fe568ccb369151823b7
Gelignite87 <railgun87@rambler.ru>
2023-04-17 20:53:44 +1300

    17.04.2023

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -7,8 +7,7 @@ const primary = "#E30B13";
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     colors: {
