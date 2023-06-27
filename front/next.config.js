76b4d3525a2b12aeac556e110f3844dfc78e471b
Gelignite87 <railgun87@rambler.ru>
2023-06-28 03:51:41 +1200

    Workflow 150

--- front/next.config.js
+++ front/next.config.js
@@ -1,24 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	// reactStrictMode: true, //отключаем строгий режим потому что не все библиотеки с ним работают
-	poweredByHeader: false, //убираем метку что сайт сделан на NEXTjs
+	poweredByHeader: false, //убираем метку что сайт сделан на NEXT
 	env: {
-		//чтобы использовать содержимое файла .env в NEXTjs
+		//чтобы использовать содержимое файла .env
 		APP_SERVER_URL: process.env.APP_SERVER_URL,
 	},
 	async rewrites() {
-		//переписываем пути чтобы localhost:3000/api переходил на localhost:4200/api
+		//редирект путей без хоста
 		return [
 			{
 				source: '/api/:path*',
-				// destination: 'http://localhost:4200/api/:path*',
-				// destination: 'http://host.docker.internal:4200/api/:path*',
 				destination: `${process.env.APP_SERVER_URL}/api/:path*`,
 			},
 			{
 				source: '/uploads/:path*',
-				// destination: 'http://localhost:4200/uploads/:path*',
-				// destination: 'http://host.docker.internal:4200/uploads/:path*',1
 				destination: `${process.env.APP_SERVER_URL}/uploads/:path*`,
 			},
 		]
