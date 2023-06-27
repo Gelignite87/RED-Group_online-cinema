e1378aa20fce68d68f5878acbe44f7df6e0d66f0
Gelignite87 <railgun87@rambler.ru>
2023-06-28 02:34:49 +1200

    Workflow 136

--- front/next.config.js
+++ front/next.config.js
@@ -7,23 +7,23 @@ const nextConfig = {
 		APP_URL: process.env.APP_URL,
 		APP_SERVER_URL: process.env.APP_SERVER_URL,
 	},
-	// async rewrites() {
-	// 	//переписываем пути чтобы localhost:3000/api переходил на localhost:4200/api
-	// 	return [
-	// 		{
-	// 			source: '/api/:path*',
-	// 			// destination: 'http://localhost:4200/api/:path*',
-	// 			// destination: 'http://host.docker.internal:4200/api/:path*',
-	// 			destination: 'http://172.23.0.2:4200/api/:path*',
-	// 		},
-	// 		{
-	// 			source: '/uploads/:path*',
-	// 			// destination: 'http://localhost:4200/uploads/:path*',
-	// 			// destination: 'http://host.docker.internal:4200/uploads/:path*',
-	// 			destination: 'http://172.23.0.2:4200/uploads/:path*',
-	// 		},
-	// 	]
-	// },
+	async rewrites() {
+		//переписываем пути чтобы localhost:3000/api переходил на localhost:4200/api
+		return [
+			{
+				source: '/api/:path*',
+				// destination: 'http://localhost:4200/api/:path*',
+				// destination: 'http://host.docker.internal:4200/api/:path*',
+				destination: 'http://172.23.0.2:4200/api/:path*',
+			},
+			{
+				source: '/uploads/:path*',
+				// destination: 'http://localhost:4200/uploads/:path*',
+				// destination: 'http://host.docker.internal:4200/uploads/:path*',
+				destination: 'http://172.23.0.2:4200/uploads/:path*',
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
