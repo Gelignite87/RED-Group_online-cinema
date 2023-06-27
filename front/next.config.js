c78be211c03428ece93a4c3b54781dccc402e708
Gelignite87 <railgun87@rambler.ru>
2023-06-27 23:31:50 +1200

    Workflow 115

--- front/next.config.js
+++ front/next.config.js
@@ -19,7 +19,7 @@ const nextConfig = {
 			},
 			{
 				source: '/uploads/:path*',
-				destination: 'http://localhost:4200/uploads/:path*',
+				// destination: 'http://localhost:4200/uploads/:path*',
 				// destination: 'http://host.docker.internal:4200/uploads/:path*',
 				// destination: 'http://176.57.213.148:4200/uploads/:path*',
 				destination: 'http://fdasite.ru:4200/uploads/:path*',
