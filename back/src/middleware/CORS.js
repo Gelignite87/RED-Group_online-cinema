597f4dbe6a1d66dbf8c36049a7f1bed759f9af82
Gelignite87 <railgun87@rambler.ru>
2023-06-27 23:47:22 +1200

    Workflow 118

--- back/src/middleware/CORS.js
+++ back/src/middleware/CORS.js
@@ -1,11 +1,8 @@
 /* Решаем проблему CORS */
 
 export const CORS = (req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.setHeader(
-		'Access-Control-Allow-Methods',
-		'GET, POST, PUT, PATCH, POST, DELETE, OPTIONS'
-	)
+	res.setHeader('Access-Control-Allow-Origin', 'http://fdasite.ru:3200/*')
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 	next()
 }
