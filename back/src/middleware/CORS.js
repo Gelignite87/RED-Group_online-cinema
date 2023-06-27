153b52aa5bc7f35ae9363145cfd1d52f76577931
Gelignite87 <railgun87@rambler.ru>
2023-06-27 23:42:46 +1200

    Workflow 117

--- back/src/middleware/CORS.js
+++ back/src/middleware/CORS.js
@@ -2,7 +2,10 @@
 
 export const CORS = (req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, PATCH, POST, DELETE, OPTIONS'
+	)
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 	next()
 }
