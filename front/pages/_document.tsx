90a628f11898a0213abd751e9c914fa16e66224e
dmitrii fomin <gelignite01@gmail.com>
2024-11-04 14:12:14 +1200

    210

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -1,13 +1,13 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
-	return (
-		<Html lang="en">
-			<Head />
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
+	return (<p>04.11.2024</p>
+		// <Html lang="en">
+		// 	<Head />
+		// 	<body>
+		// 		<Main />
+		// 		<NextScript />
+		// 	</body>
+		// </Html>
 	)
 }
